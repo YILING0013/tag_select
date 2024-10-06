16db97d1f02de79efb98328c214a965b5ad0fed3
YILING0013 <xielingqing123@gmail.com>
2024-10-06 21:51:24 +0800

    修改Tag表

--- tag_select.py
+++ tag_select.py
@@ -1,20 +1,20 @@
 from flask import Blueprint, jsonify
-import yaml
+import json
 import os
 
 tag_select_blueprint = Blueprint('tag_select', __name__)
 
-def load_yaml_data(file_name):
-    file_path = os.path.join('tag_yaml', f'{file_name}.yaml')
+def load_json_data(file_name):
+    file_path = os.path.join('tag_json', f'{file_name}.json')
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
-            return yaml.safe_load(file)
+            return json.load(file)
     except FileNotFoundError:
         return None
 
 @tag_select_blueprint.route('/get_tags/<category>')
 def get_tags(category):
-    data = load_yaml_data(category)
+    data = load_json_data(category)
     if data is not None:
         return jsonify(data)
     else:
